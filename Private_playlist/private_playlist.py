69b3f5257b5522f5da9927179206dbe162e958d3
Avhi Aryal <avhiaryal54@gmail.com>
2021-01-12 16:13:02 +0545

    Update private_playlist.py

--- Private_playlist/private_playlist.py
+++ Private_playlist/private_playlist.py
@@ -11,7 +11,7 @@ query = f'{endpoint_url}limit={limit}&market={market}&seed_genres={seed_genres}&
 
 response =requests.get(query, 
                headers={"Content-Type":"application/json", 
-                        "Authorization":"BQBLUaVC44OmOFeMBt81ijsb7YopwNCcHNht0EZo9b065VHkFXt_G_kKdF5IpS9YATHjAdNUSXKHdCsWQmgU0h83toOlUbUzP3hE9GX_H68VqBbFb0yC-GL0PIW3hFW6NQPhuSBLC6lLtHbaX2rQ6gbFN7R1Ooau5vSlzPdvq8nMWN8XbgFmaGJ2iGcGZK4"})
+                        "Authorization":" Bearer BQBLUaVC44OmOFeMBt81ijsb7YopwNCcHNht0EZo9b065VHkFXt_G_kKdF5IpS9YATHjAdNUSXKHdCsWQmgU0h83toOlUbUzP3hE9GX_H68VqBbFb0yC-GL0PIW3hFW6NQPhuSBLC6lLtHbaX2rQ6gbFN7R1Ooau5vSlzPdvq8nMWN8XbgFmaGJ2iGcGZK4"})
 
 json_response = response.json()
 
